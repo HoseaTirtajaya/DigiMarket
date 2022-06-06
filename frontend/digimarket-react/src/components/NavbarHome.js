5104854a3ce1874a19ae7fe4928aca17ada5591b
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2022-06-06 14:48:42 +0700

    Still error react bootstrap

--- frontend/digimarket-react/src/components/NavbarHome.js
+++ frontend/digimarket-react/src/components/NavbarHome.js
@@ -4,25 +4,16 @@ import Container from "react-bootstrap/Container";
 
 export default function NavbarHome() {
   return (
-    // <Navbar>
-    //     <Container>
-    //         <Navbar.Brand>Navbar with text</Navbar.Brand>
-    //         <Navbar.Toggle />
-    //         <Navbar.Collapse className="justify-content-end">
-    //         <Navbar.Text>
-    //             Signed in as: <a href="#login">Mark Otto</a>
-    //         </Navbar.Text>
-    //         </Navbar.Collapse>
-    //     </Container>
-    // </Navbar>
-    <>
-    <Container>
-      <Navbar expand="lg" variant="light" bg="light">
+    <Navbar>
         <Container>
-          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+            <Navbar.Brand>Navbar with text</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+                Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+            </Navbar.Collapse>
         </Container>
-      </Navbar>
-    </Container>
-    </>
+    </Navbar>
   )
 }
